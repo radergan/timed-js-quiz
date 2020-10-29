3b54f2d4057f5d26c65c1adceef8b2c47f73f85d
Patrick J Dalton <pdalton@aha.org>
2020-10-28 20:42:26 -0500

    Fixed bugs, added functionality for penalties to time when wrong, improved some style elements

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ var qanda = [
 		choices: [
     	    {text: "Not available now", correct:false},
             {text: "Nothing available next", correct:false},
-            {text: "Not a number", correct:true}
+			{text: "Not a number", correct:true}
         ]
     },
 	{	
@@ -37,13 +37,61 @@ var qanda = [
         ]
     }
 ];
-
+//set the default time to 59 seconds
+//on click begin, start the timer
+//on clicking an answer, if its wrong, subtract 10 seconds from timer
 var counter = 0;
 var correctAnswerNumber = -1;
-var secondsLeft = 59;
+var timeCounter = 59;
+var penaltyWhenWrong = 10; //minus 10 pts
+$("#penalty-temp").html("Wrong answer:  - " + penaltyWhenWrong + " seconds.");
 
-function generateSomeStuff(){
+$("#start-the-quiz").click(function(){
+    $(this).hide();
+	$("#initial-instructions").hide();
+	setTime(timeCounter);
+});
+	
+function setTime(x){
+	x = timeCounter;
+	x = setInterval(function(){
+	
+	$("#tick-tock-box").text(timeCounter);
+      
+	if(timeCounter === 0){
+		generateResultScreen();
+	}
+	timeCounter -= 1;
+	}, 1000);
+}
+
+$("#list-of-answers").on('click', '.answer-button', function(e) {
+    e.stopImmediatePropagation();
     
+    if (correctAnswerNumber == $(this).attr("data-qnum")){
+        $("#right-or-wrong").html("<div class='alert alert-success'>Correct answer!</div>");
+    }
+    else{
+        $("#right-or-wrong").html("<div class='alert alert-danger'>Wrong answer!</div>");
+		timeCounter = timeCounter - penaltyWhenWrong;
+		displayPenalty();
+    }
+	
+    if (counter != qanda.length){
+        questionsAndAnswers();
+		
+    }else{;
+        generateResultScreen();
+    }
+});
+
+function displayPenalty (){
+	var penaltyBox = $("#penalty-temp");
+	penaltyBox.show();
+	penaltyBox.fadeOut(3000);
+}
+
+function questionsAndAnswers(){
     $("#list-of-answers").html("");
     var currentQuestionTitle = qanda[counter].question;
     var listOfAnswers = qanda[counter].choices;
@@ -56,7 +104,6 @@ function generateSomeStuff(){
         var isThisCorrect = qanda[counter].choices[i].correct;
         if(isThisCorrect === true){
             correctAnswerNumber = i;
-            console.log(correctAnswerNumber);
         }
 
         answerContainer.append("<button class='answer-button btn btn-primary' data-qnum='" + i + "'>" + thisAnswerText + "</button>");
@@ -65,24 +112,35 @@ function generateSomeStuff(){
     counter = counter + 1;
 }
 
+function generateResultScreen(){
+    $("#tick-tock-box").hide();
+    $("#heres-the-choices").hide();
+	var quizResults = "<div class='alert alert-warning'>Time has expired. You finished the quiz with a score of " + timeCounter + ".</div>";
+	$("#quiz-body").append(quizResults);
+	
+    var initials = prompt("Enter your initials");
+    var score = timeCounter;
+    var allScores = JSON.parse(addNewScore(initials, score));
 
-$("#list-of-answers").on('click', '.answer-button', function(e) {
-    e.stopImmediatePropagation();
-    
-    if (correctAnswerNumber == $(this).attr("data-qnum")){
-        $("#right-or-wrong").html("<div class='alert alert-success'>Correct answer!</div>");
-    }
-    else{
-        $("#right-or-wrong").html("<div class='alert alert-danger'>Wrong answer!</div>");
-    }
+    printScores(allScores);
+	
+    var endButtons = "<button class='btn btn-primary' onclick='location.reload();'>Try quiz again</button>";
+	
+    endButtons = endButtons + "<button class='btn btn-primary' onclick='clearScores()'>Clear scores</button>";
+    $("#quiz-body").append("<p id='try-again'>" + endButtons + "</p>");
+}
 
-    if (counter != qanda.length){
-        generateSomeStuff();
-    }else{;
-        generateResultScreen();
-    }
+function printScores(x){
+	var allScores = x;
+	var scoreTable = "<table class='table' id='table-of-scores'><thead><tr><td>Initials</td><td>Score</td></tr></thead>";
 
-});
+    for(var i = 0; i < allScores.length; i++){
+        var tableRow = "<tr><td>" + allScores[i].initials + "</td><td>" + allScores[i].score + "</td></tr>";
+        var scoreTable = scoreTable + tableRow;
+    }
+    scoreTable = scoreTable + "</table>";
+    $("#quiz-body").append(scoreTable);
+}
 
 function addNewScore (initials, score) {
     var players = JSON.parse(localStorage.getItem('playerScores')) || [];
@@ -95,47 +153,3 @@ function clearScores(){
     localStorage.clear();
 	$("#table-of-scores").html("");
 }
-function printScores(x){
-	var allScores = x;
-	var scoreTable = "<table class='table' id='table-of-scores'><thead><tr><td>Initials</td><td>Score</td></tr></thead>";
-
-    for(var i = 0; i < allScores.length; i++){
-        var tableRow = "<tr><td>" + allScores[i].initials + "</td><td>" + allScores[i].score + "</td></tr>";
-        var scoreTable = scoreTable + tableRow;
-    }
-    scoreTable = scoreTable + "</table>";
-    $("#quiz-body").append(scoreTable);
-}
-function generateResultScreen(){
-    $("#tick-tock-box").hide();
-    $("#heres-the-choices").hide();
-	var quizResults = "<div class='alert alert-warning'>Time has expired. You finished the quiz with a score of " + secondsLeft + ".</div>";
-	$("#quiz-body").append(quizResults);
-	
-    var initials = prompt("Enter your initials");
-    var score = secondsLeft;
-    var allScores = JSON.parse(addNewScore(initials, score));
-
-    printScores(allScores);
-	
-    var endButtons = "<button class='btn btn-primary' onclick='location.reload();'>Try quiz again</button>";
-	
-    endButtons = endButtons + "<button class='btn btn-primary' onclick='clearScores()'>Clear scores</button>";
-    $("#quiz-body").append("<p id='try-again'>" + endButtons + "</p>");
-}
-
-
-$("#start-the-quiz").click(function(){
-    $(this).hide();
-	$("#initial-instructions").hide();
-    quizTimer = setInterval(function(){
-
-    $("#tick-tock-box").text(secondsLeft);
-      
-    if(secondsLeft === 0){
-        clearInterval(quizTimer);
-        generateResultScreen();
-    }
-    secondsLeft -= 1;
-    }, 1000);
-});
